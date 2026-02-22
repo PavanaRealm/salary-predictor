14b01804a48dc47bc3f79afff35d3874e82f4c4c
Pavani Bhuvaneswari <pavanibhuvaneswariyalla@gmail.com>
2026-02-22 14:16:50 +0100

    Add Berlin location and improve model

--- app.py
+++ app.py
@@ -1,23 +1,53 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pickle
 import pandas as pd
 
 app = Flask(__name__)
 
-# Load the saved model
-with open('salary_model.pkl', 'rb') as f:
+# Load enhanced model and encoders
+with open('salary_model2.pkl', 'rb') as f:
     model = pickle.load(f)
+
+with open('encoders.pkl', 'rb') as f:
+    encoders = pickle.load(f)
+
+job_titles = list(encoders['job_title'].classes_)
+locations = list(encoders['location'].classes_)
+educations = list(encoders['education'].classes_)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', 
+                           job_titles=job_titles,
+                           locations=locations,
+                           educations=educations)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     experience = float(request.form['experience'])
-    prediction = model.predict(pd.DataFrame({'years_experience': [experience]}))[0]
+    job_title = request.form['job_title']
+    location = request.form['location']
+    education = request.form['education']
+
+    job_encoded = encoders['job_title'].transform([job_title])[0]
+    location_encoded = encoders['location'].transform([location])[0]
+    education_encoded = encoders['education'].transform([education])[0]
+
+    input_data = pd.DataFrame([[experience, job_encoded, location_encoded, education_encoded]],
+                              columns=['years_experience', 'job_title_encoded', 'location_encoded', 'education_encoded'])
+
+    prediction = model.predict(input_data)[0]
     salary = f"${prediction:,.0f}"
-    return render_template('index.html', prediction=salary, experience=experience)
+
+    return render_template('index.html',
+                           prediction=salary,
+                           experience=experience,
+                           job_title=job_title,
+                           location=location,
+                           education=education,
+                           job_titles=job_titles,
+                           locations=locations,
+                           educations=educations)
 
 if __name__ == '__main__':
     app.run(debug=True)
